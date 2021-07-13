bb609ec9c5315b39251855e63deb9b5235aa889e
Tate Mowbray <tatemowbray@outlook.com>
2021-07-13 15:48:41 -0700

    Display component added to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Layout } from 'antd';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
@@ -8,6 +8,15 @@ import './index.css';
 import logo from './thumbs-up-pip.png';
 
 const { Header, Content, Footer } = Layout;
+function Display() {
+  const [currentTab, setCurrentTab] = useState(<Welcome />);
+
+  return(
+    <div>
+      {currentTab}
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,7 +28,7 @@ ReactDOM.render(
         <StandardMenu />
       </Header>
       <Content style={{ padding: '0 10vh', height: "calc(100vh - 10vh - 7vh)" }}>
-        <Welcome />
+        <Display />
       </Content>
       <Footer style={{ textAlign: 'center', backgroundColor: "yellow", height: "10vh" }}>
         Just another footer. 
