c7f77c2488dbabc5d58f257eb611b20eab60631b
Kyle <kylebisley@gmail.com>
2021-07-13 14:48:58 -0700

    pipboy

--- src/index.js
+++ src/index.js
@@ -5,21 +5,24 @@ import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import Welcome from './Welcome.js';
 import StandardMenu from './StandardMenu.js';
 import reportWebVitals from './reportWebVitals';
-import './index.css'
+import './index.css';
+import logo from './thumbs-up-pip.png';
 
 const { Header, Content, Footer } = Layout;
 
 ReactDOM.render(
   <React.StrictMode>
     <Layout className="layout">
-      <Header>
-        <div className="logo" />
+      <Header style={{ height: "8vh"}}>
+          <div>
+            <img src={logo} alt="Thumbs Up Pipboy" className="logo" style={{height:'6vh', position: 'absolute', bottom:'0' }} />
+          </div>
         <StandardMenu />
       </Header>
-      <Content style={{ padding: '0 10vh' }}>
+      <Content style={{ padding: '0 10vh', height: "calc(100vh - 10vh - 8vh)" }}>
         <Welcome />
       </Content>
-      <Footer style={{ textAlign: 'center' }}>
+      <Footer style={{ textAlign: 'center', backgroundColor: "yellow", height: "10vh" }}>
         Just another footer. 
       </Footer>
     </Layout>
