9876cead5a3fd83b04df397655b6a6b410486672
Kyle <kylebisley@gmail.com>
2021-07-13 16:20:48 -0700

    Sponsor slide further fleshed out

--- src/AboutSponsor.js
+++ src/AboutSponsor.js
@@ -9,12 +9,13 @@ class AboutSponsor extends React.Component {
     render() {
         return (
         <Space direction="vertical horizontal">
-            <Card title="Tate" style={{ padding: '1vh 1vw 1vh 1vw' }}>
+            <Card title="Project Sponsored by Cuboh" style={{ padding: '1vh 1vw 1vh 1vw' }}>
                 <Space direction="horizontal">
-                    <img src={cubohLogo} alt="Thumbs Up Pipboy" className="logo" style={{height:'7vh', float:'left' }} />
+                    <a href="https://www.cuboh.com/">
+                        <img src={cubohLogo} alt="Cuboh Logo" className="logo" style={{height:'10vh', witdh:'automatic', float:'left' }} />
+                    </a>
                     <Space direction="vertical">
-                        <p>Tate has been a Camosun student since 2015. During his time in the Camosun ICS program, he has learned fundamental programming practices, earned experience working as a web developer, and made excellent friends to enjoy it with. Tate enjoys working with front and back-end development. In his free time, Tate works on his professional development, home projects, and spends time with his family.</p>
-                        <p>Card content</p>
+                        <p>Cuboh aggregates online orders from food delivery apps through a single device to greatly simplify restaurant’s workflows. The Menu Scraper project will generate a free evaluative report of a restaurant’s menu, as hosted by various food delivery apps. The report can then be leveraged to enhance the restaurant’s online presence. Additionally, the user’s email will be shared with Cuboh’s marketing department to help inform potential customers of other ways to improve their online experience.</p>
                     </Space>
                 </Space>
             </Card>
