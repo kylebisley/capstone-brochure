b6e5f34ae1f8eae7c5727c09aa06febecb9c22de
Kyle <kylebisley@gmail.com>
2021-07-13 11:47:41 -0700

    welcome page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
 function App() {
   return (
     <div className="App">
---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+function Welcome() {
+    return (
+        <Row>
+            <Col>
+                <h1>Welcome</h1>
+            </Col>
+        </Row>
+    );
+}
+
+export default Welcome;
