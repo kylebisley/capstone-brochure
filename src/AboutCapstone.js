623cf6fd518a41d068cf65208dbe6ae650b1ee24
Kyle <kylebisley@gmail.com>
2021-07-21 09:27:08 -0700

    couple of checked spelling (by kyle so not very checked)

--- src/AboutCapstone.js
+++ src/AboutCapstone.js
@@ -15,7 +15,7 @@ class AboutCapstone extends React.Component {
                     <img  src={camosun} alt="Camosun Logo" style={{height:'25vh', width:'automatic', justifyContent:'center' }} />
                     <br />
                     <p style={{textAlign:'left' }}>The Camosun College Computer Science Programs are designed to foster professional software developers, analysts and support technologists. Through the Capstone Project process, students work directly with an industry sponsor to solidify their knowledge and expertise in the area of software application development.</p>
-                    <p style={{textAlign:'left' }}>Over a period of four months, our second-year students work in teams analyzing, designing, and implementing a software solution of a project proposed by a variety of organizations. At the end of the term, the student teams present their projects at the Capstone Symposium, a formal trade show for the community. Students may work off-site, at your organization, during the project.</p>
+                    <p style={{textAlign:'left' }}>Over a period of four months, second-year students work in teams analyzing, designing, and implementing a software solution of a project proposed by a variety of organizations. At the end of the term, the student teams present their projects at the Capstone Symposium, a formal trade show for the community. Students may work off-site, at sponsor organizations, during the project. This year it has been held 100% remote.</p>
                 </Space>
             </Card>
         </Space>
--- src/Welcome.js
+++ src/Welcome.js
@@ -11,22 +11,30 @@ class Welcome extends React.Component {
                 <Space direction="horizontal">
                     <div style={{width: '30vw'}}>
                         <h1>Welcome</h1>
-                        <h2>What is Capstone?</h2>
+                        <h2>Opening Sequence</h2>
                         <Space style={{textAlign:'left' }} direction="vertical">
                             <h3>We're Capstone Team D, also know as Team Cuboh</h3>
                             <p>You've found our brochure website for Capstone 2021.</p>
-                            <p>This project was commissioned by Cuboh Inc. to create an application that would allow restaurants to get useful feedback on the quality of their online menus as hosted by various food ordering platforms.</p>
+                            <p>Team Cuboh’s project was commissioned by Cuboh Inc. to create an application to provide restaurants useful feedback on their online menus hosted on food ordering platforms. It is a react website with a flask backend API. The project is presently on schedule for a successful, timely, delivery.</p>
                         </Space>
                     </div>
                     <Space style={{textAlign:'left' }} direction="vertical">
+                        <figure>
                             <video loop muted autoPlay height="500px" style={{float:'right'}} >
                                 <source src={App_preview} type="video/mp4" />
                             </video>
+                            <figcaption>
+                                Figure 1: A demonstration of our application.
+                            </figcaption>
+                        </figure>
                     </Space>
                 </Space>
                 <Space style={{textAlign:'left' }} direction="horizontal">
+                    <Space style={{textAlign:'left' }} direction="vertical">
+                        <h3>What does the Menu Scraper App do? </h3>
                         <p>Cuboh asked for a website that will collect marketing leads on local restaurants who maintain a presence in online food ordering platforms. Specifically, this website should attract restaurants with a presence on the big five platforms: Uber Eats, Skip the Dishes, Door Dash Grubhub and Chow Now. The application will generate feedback on a restaurant’s online menu by scraping the websites and emailing a report to the local restaurateur. In addition, it will send the email address to the Cuboh marketing team as sales leads.</p>
                     </Space>
+                </Space>
                 </Card>
             </Space>
         );
