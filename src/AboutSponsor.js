1abd75ea20a8a40aeaaa19e151e3c6d58d208263
Tate Mowbray <tatemowbray@outlook.com>
2021-07-21 17:55:17 -0700

    spelling

--- src/AboutSponsor.js
+++ src/AboutSponsor.js
@@ -19,7 +19,7 @@ class AboutSponsor extends React.Component {
                     <p>Cuboh aggregates online orders from food delivery apps through a single device to greatly simplify restaurant’s workflows. The Menu Scraper project will generate a free evaluative report of a restaurant’s menu, as hosted by various food delivery apps. The report can then be leveraged to enhance the restaurant’s online presence. Additionally, the user’s email will be shared with Cuboh’s marketing department to help inform potential customers of other ways to improve their online experience.</p>
                     <h2>Special Thanks</h2>
                     <p>Team D would like to thank Cuboh, specifically Samreeta Kandola and Manny Pasquale for their continued support and presence. In addition we would like to thank Brandon and Juan for providing us this opportunity.</p>
-                    <p>We would also like to thank our instructors Ben Leather, Jonas Bambi, Katie-Tanigawa and Saryta Schaerer for their weekly check-ins, helpful direction, and kind words.</p>
+                    <p>We would also like to thank our instructors Ben Leather, Jonas Bambi, Katie Tanigawa and Saryta Schaerer for their weekly check-ins, helpful direction, and kind words.</p>
                 </Space>
             </Card>
         </Space>
